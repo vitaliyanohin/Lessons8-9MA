e95e487d7697e48f3ee570aa2918cb43bc34bc46
Vitaliy <vitaliyanohin@gmail.com>
2019-06-11 15:13:06 +0300

    developLinkedList

--- src/Application.java
+++ src/Application.java
@@ -1,19 +1,33 @@
 public class Application {
 
   public static void main(String[] args) {
-    LinkedListTest<Integer> linkedListTest = new LinkedListTest<>();
-    linkedListTest.add(0);
-    linkedListTest.add(1);
-    linkedListTest.add(2);
-    linkedListTest.add(3);
-    System.out.println(linkedListTest.get(0));
-    System.out.println(linkedListTest.get(1));
-    System.out.println(linkedListTest.get(2));
-    System.out.println(linkedListTest.get(3));
-    linkedListTest.remove(2);
-    System.out.println("_____________");
-    for (int i = 0; i < linkedListTest.size(); i++) {
-      System.out.println(linkedListTest.get(i));
+    LinkedListTest<Integer> linkedListTestOneFirst = new LinkedListTest<>();
+    LinkedListTest<Integer> linkedListTestSecond = new LinkedListTest<>();
+    linkedListTestOneFirst.add(0);
+    linkedListTestOneFirst.add(1);
+    linkedListTestOneFirst.add(2);
+    linkedListTestOneFirst.add(3);
+    linkedListTestSecond.add(4);
+    linkedListTestSecond.add(5);
+    System.out.println("Initial main List:");
+    System.out.println(linkedListTestOneFirst.get(0));
+    System.out.println(linkedListTestOneFirst.get(1));
+    System.out.println(linkedListTestOneFirst.get(2));
+    System.out.println(linkedListTestOneFirst.get(3));
+    linkedListTestOneFirst.remove(1);
+//    linkedListTestOneFirst.remove(0);
+//    linkedListTestOneFirst.remove(3);
+//    linkedListTestOneFirst.remove(4);
+//    linkedListTestOneFirst.remove(Integer.valueOf(2));
+    linkedListTestOneFirst.add(11, 0);
+//    linkedListTestOneFirst.add(11, 2);
+//    linkedListTestOneFirst.add(11, 3);
+//    linkedListTestOneFirst.add(11, 4);
+    linkedListTestOneFirst.addAll(linkedListTestSecond);
+    linkedListTestOneFirst.set(22, 3);
+    System.out.println("Changes in main List:");
+    for (int i = 0; i < linkedListTestOneFirst.size(); i++) {
+      System.out.println(linkedListTestOneFirst.get(i));
     }
   }
 }
