a349540e20f534ebf2abe2807d504f19ca3b19a3
Vitaliy <vitaliyanohin@gmail.com>
2019-06-11 15:15:29 +0300

    developLinkedList

--- src/Application.java
+++ src/Application.java
@@ -1,33 +1,33 @@
 public class Application {
 
   public static void main(String[] args) {
-    LinkedListTest<Integer> linkedListTestOneFirst = new LinkedListTest<>();
+    LinkedListTest<Integer> linkedListTestFirst = new LinkedListTest<>();
     LinkedListTest<Integer> linkedListTestSecond = new LinkedListTest<>();
-    linkedListTestOneFirst.add(0);
-    linkedListTestOneFirst.add(1);
-    linkedListTestOneFirst.add(2);
-    linkedListTestOneFirst.add(3);
+    linkedListTestFirst.add(0);
+    linkedListTestFirst.add(1);
+    linkedListTestFirst.add(2);
+    linkedListTestFirst.add(3);
     linkedListTestSecond.add(4);
     linkedListTestSecond.add(5);
     System.out.println("Initial main List:");
-    System.out.println(linkedListTestOneFirst.get(0));
-    System.out.println(linkedListTestOneFirst.get(1));
-    System.out.println(linkedListTestOneFirst.get(2));
-    System.out.println(linkedListTestOneFirst.get(3));
-    linkedListTestOneFirst.remove(1);
-//    linkedListTestOneFirst.remove(0);
-//    linkedListTestOneFirst.remove(3);
-//    linkedListTestOneFirst.remove(4);
-//    linkedListTestOneFirst.remove(Integer.valueOf(2));
-    linkedListTestOneFirst.add(11, 0);
-//    linkedListTestOneFirst.add(11, 2);
-//    linkedListTestOneFirst.add(11, 3);
-//    linkedListTestOneFirst.add(11, 4);
-    linkedListTestOneFirst.addAll(linkedListTestSecond);
-    linkedListTestOneFirst.set(22, 3);
+    System.out.println(linkedListTestFirst.get(0));
+    System.out.println(linkedListTestFirst.get(1));
+    System.out.println(linkedListTestFirst.get(2));
+    System.out.println(linkedListTestFirst.get(3));
+    linkedListTestFirst.remove(1);
+//    linkedListTestFirst.remove(0);
+//    linkedListTestFirst.remove(3);
+//    linkedListTestFirst.remove(4);
+//    linkedListTestFirst.remove(Integer.valueOf(2));
+    linkedListTestFirst.add(11, 0);
+//    linkedListTestFirst.add(11, 2);
+//    linkedListTestFirst.add(11, 3);
+//    linkedListTestFirst.add(11, 4);
+    linkedListTestFirst.addAll(linkedListTestSecond);
+    linkedListTestFirst.set(22, 3);
     System.out.println("Changes in main List:");
-    for (int i = 0; i < linkedListTestOneFirst.size(); i++) {
-      System.out.println(linkedListTestOneFirst.get(i));
+    for (int i = 0; i < linkedListTestFirst.size(); i++) {
+      System.out.println(linkedListTestFirst.get(i));
     }
   }
 }
